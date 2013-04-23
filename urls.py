7711ef963a28ea00d9a49cfc4e79bef6696c69f4
matsinvasion <markmusasizi@gmail.com>
2013-04-23 11:30:24 +0200

    fixing bug in urls

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-from django.contrib import admin
+from django.conf.urls.defaults import patterns, include, url
+#from django.views.generic.simple import direct_to_template
+#from django.contrib import admin
 from djangoratings.views import AddRatingFromModel
 import settings
 
 # Uncomment the next two lines to enable the admin:
- from django.contrib import admin
- admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 #	url(r'^$', direct_to_template, {'template': 'index.html'}),
