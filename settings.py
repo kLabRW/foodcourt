f96dada675b7bb4096122d1fbb6653bd9a865e08
matsinvasion <markmusasizi@gmail.com>
2013-04-25 20:24:03 +0200

    fixing debug bug

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-ALLOWED_HOSTS = ['f4l.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
