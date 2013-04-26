9e1e84042f6b309dee79fd485ccc6267459641d9
matsinvasion <markmusasizi@gmail.com>
2013-04-26 15:17:44 +0200

    need to hook up S3 to serve images,its failing for now

--- restaurant_detail/views.py
+++ restaurant_detail/views.py
@@ -40,7 +40,7 @@ class Restaurant_detailCRUDL(SmartCRUDL):
 	
    	class List(SmartListView):
 		fields = ('restaurant_name','first_name','last_name','mobile','cusine', 'service_type','service_fee','service_days','service_hours_start','service_hours_end','minimum_order_amount','delivery_territory')
-		search_fields =('restaurant_name__icontains','cusine__icontains','services__icontains',)
+		search_fields =('restaurant_name__icontains','cusine__icontains',)
 		field_config = {'services': dict(label = "SERVICES")}
 		
 		def derive_queryset(self, **kwargs):
@@ -79,7 +79,7 @@ class Restaurant_detailCRUDL(SmartCRUDL):
 #		def get_delivery_days(self,obj):
 #			return obj.get_delivery_days_display()
 	class Update(SmartUpdateView):
-		fields=('restaurant_name','first_name','last_name','mobile','cusine', 'service_type','service_fee','service_days','service_hours_start','service_hours_end','minimum_order_amount','delivery_territory','is_active')
+		fields=('restaurant_name','first_name','last_name','logo','mobile','cusine', 'service_type','service_fee','service_days','service_hours_start','service_hours_end','minimum_order_amount','delivery_territory','is_active')
 	class Create(SmartCreateView):
 		permission=None
 		submit_button_name="Join the F4L restaurant network"
