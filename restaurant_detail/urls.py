78dd2628f47b38d02fd5184c559b0e7217781cbb
matsinvasion <markmusasizi@gmail.com>
2013-09-12 12:03:57 +0200

    add new ToppingsAndExtras urls

--- restaurant_detail/urls.py
+++ restaurant_detail/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from .views import *
+from optionalitems.views import OptionalItemCategoryCRUDL,Optional_ItemCRUDL,ToppingsAndExtrasCategoryCRUDL,ToppingsAndExtrasCRUDL
 
 activation = RestaurantCRUDL().view_for_action('activate').as_view()
 #new = RestaurantCRUDL().view_for_action('new').as_view()
@@ -9,6 +10,11 @@ urlpatterns = Restaurant_detailCRUDL().as_urlpatterns()
 urlpatterns += RestaurantCRUDL().as_urlpatterns()
 urlpatterns += CategoryCRUDL().as_urlpatterns()
 urlpatterns += ItemCRUDL().as_urlpatterns()
+urlpatterns += OptionalItemCategoryCRUDL().as_urlpatterns()
+urlpatterns += Optional_ItemCRUDL().as_urlpatterns()
+urlpatterns += ToppingsAndExtrasCategoryCRUDL().as_urlpatterns()
+urlpatterns += ToppingsAndExtrasCRUDL().as_urlpatterns()
 urlpatterns += patterns('/^restaurant_detail/', url(r'/activate/(?P<token>\w+)/$',activation, name='url_activation'),)
+ 
 
 #urlpatterns += patterns('/^restaurant_detail/', url(r'/new/$', new, name='url_new'),)
