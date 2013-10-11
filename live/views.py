e92adacf12f1f4824ef8f319fd43a5179537e124
matsinvasion <markmusasizi@gmail.com>
2013-10-11 09:59:27 +0200

    add email notifcations for incoming order

--- live/views.py
+++ live/views.py
@@ -67,7 +67,14 @@ def show_checkout(request,id):
 			if order_number:
 				request.session['order_number'] = order_number
 			if postdata['submit'] == 'complete order':
-				reciept_url = urlresolvers.reverse('checkout_reciept')
+				"""send email notification of incoming order"""
+				recipients = ['food4lessrwanda@gmail.com']
+				order_id = str(order_created.id)
+				title = 'Order Recieved, number %s' % order_id
+				name = form.cleaned_data['name']
+				send_mail(title,name,order_id, recipients)
+				reciept_url = urlresolvers.reverse('checkout_reciept') 
+				# redirect at this point to reciept page.
 				return HttpResponseRedirect(reciept_url)
 		else:
 			order_created = None
