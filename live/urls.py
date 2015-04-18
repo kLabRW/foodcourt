deee14b35fd907a77b9f588dffe0bbde02a7daf5
mark musasizi <markmusasizi@gmail.com>
2015-04-18 12:17:11 +0200

    uncomment line

--- live/urls.py
+++ live/urls.py
@@ -3,18 +3,18 @@ from django.conf.urls.defaults import patterns, include, url
 
 
 urlpatterns = patterns('',
-	url(r'^$',maintence,name="maintenance"),
+#	url(r'^$',maintence,name="maintenance"),
 #	url(r'^$',homepage,name="homepage"),
-#   url(r'^$',restaurant_list,name="restaurant_list"),
-#	url(r'^menu/(\d+)$',get_category, name="f4l_menu"),
-#	url(r'^your_order/(?P<id>\d+)/$',show_order,name="order_index"),
-#	url(r'^item/(?P<id>\d+)/$',show_item, name="item_order"),
-#	url(r'^checkout/(?P<id>\d+)/$',show_checkout,name="checkout"),
-#	url(r'^reciept/$',reciept,name="checkout_reciept"),
-#	url(r'^search_results/$',search,name="search"),
-#	url(r'^contact/$',contact,name="contact_us"),
+   url(r'^$',restaurant_list,name="restaurant_list"),
+	url(r'^menu/(\d+)$',get_category, name="f4l_menu"),
+	url(r'^your_order/(?P<id>\d+)/$',show_order,name="order_index"),
+	url(r'^item/(?P<id>\d+)/$',show_item, name="item_order"),
+	url(r'^checkout/(?P<id>\d+)/$',show_checkout,name="checkout"),
+	url(r'^reciept/$',reciept,name="checkout_reciept"),
+	url(r'^search_results/$',search,name="search"),
+	url(r'^contact/$',contact,name="contact_us"),
 #	url(r'^how_it_works/$',how_it_works_page,name="how_it_works"),
-#	url(r'^captcha/',include('captcha.urls')),
+	url(r'^captcha/',include('captcha.urls')),
 
 #	url(r'^homepage$',homepage,name="homepage"),
 #	url(r'^form/$',tryout_form,name="tryout")
